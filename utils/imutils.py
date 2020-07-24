bf7ad1688cd1dd01fa7fe3ec9bb3777676f4c560
dron-dronych <dr_dronych@mail.ru>
2020-07-24 18:33:10 +0600

    init commit: added img utils

---
+++ utils/imutils.py
@@ -0,0 +1,37 @@
+import random
+import pydicom
+
+
+def load_dicom(img_path):
+    """
+    load image in DICOM format
+    """
+    dataset = pydicom.dcmread(img_path)
+
+    if 'PixelData' in dataset:
+        rows = int(dataset.Rows)
+        cols = int(dataset.Columns)
+
+        print('Image size: {}x{}'.format(
+            rows, cols
+        ))
+
+    plt.imshow(dataset.pixel_array)
+
+
+def load_random_dicoms(dicom_dir, n_imag=5, seed=22):
+    """"""
+    random.seed(seed)  # for reproducability
+    counter = 0
+
+    imgs = glob.glob(dicom_dir + '/*/*.dcm')
+    plt.figure(figsize=(14, 14))
+
+    while counter < n_imag:
+        im = random.choice(imgs)
+
+        plt.subplot(2, 3, counter + 1)
+
+        load_dicom(im)
+
+        counter += 1
