966291649dfff48b98abd9d9f328012f070b2e2b
dron-dronych <dr_dronych@mail.ru>
2020-09-10 17:20:45 +0600

    simple neural network

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 
 def laplace_log_likelihood(y_true, y_pred):
@@ -18,3 +20,31 @@ def laplace_log_likelihood(y_true, y_pred):
     metric = -(tf.math.sqrt(2.0) * delta / sigma_clipped) - tf.math.log(tf.math.sqrt(2.0) * sigma_clipped)
 
     return metric
+
+
+def build_model(input_shape):
+    inputs = layers.Input(shape=input_shape)
+    x = layers.Dense(256, activation='relu')(inputs)
+    x = layers.Dense(128, activation='relu')(x)
+    x = layers.Dense(64, activation='relu')(x)
+    preds = layers.Dense(1, activation='linear')(x)
+
+    # this adds a confidence (constant = 100) to each prediction
+    @tf.function
+    def constant(tensor):
+        batch_size = tf.shape(tensor)[0]
+        constant_ = tf.constant(100)
+        return tf.broadcast_to(constant_, shape=(batch_size, 1))
+
+    confidence = keras.layers.Lambda(constant)(inputs)
+
+    model = keras.Model(inputs=[inputs],
+                        outputs=[preds, confidence]
+                        )
+
+    model.compile(optimizer='adam',
+                  loss=keras.losses.MeanAbsoluteError(),
+                  metrics=laplace_log_likelihood
+                  )
+
+    return model
