b917031eb9ff78fcb85fd98fe0e38e6717c91f0c
dron-dronych <dr_dronych@mail.ru>
2020-09-11 22:21:41 +0600

    initialized cli to train a model

--- train.py
+++ train.py
@@ -1,5 +1,12 @@
 import utils.imutils as ut
+import argparse
 
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-d', '--dataset', help='path to directory with CT scans')
+ap.add_argument('-m', '--model', help='path to save output model')
+
+args = ap.parse_args()
 
 
 
