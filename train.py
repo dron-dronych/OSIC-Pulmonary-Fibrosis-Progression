ecb911dc830896fe302bff2abf0ca1f21085b845
dron-dronych <dr_dronych@mail.ru>
2020-09-17 22:29:28 +0600

    added simple training script

--- train.py
+++ train.py
@@ -1,10 +1,35 @@
-# import utils.imutils as ut
+import utils.imutils as iut
+import utils.utils as utils
 import argparse
+import pandas as pd
 
-ap = argparse.ArgumentParser()
-ap.add_argument('-d', '--dataset', help='path to directory with CT scans',
-                required=True
-                )
-ap.add_argument('-m', '--model', help='path to save output model')
 
-args = ap.parse_args()
+def setup_cli():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-d', '--dataset', help='path to base directory with CT scans',
+                    required=True
+                    )
+    ap.add_argument('-m', '--model', help='path to save output model')
+
+    return vars(ap.parse_args())
+
+def read_fvc_data(path):
+    return pd.read_csv(path)
+
+
+if __name__ == '__main__':
+    args = setup_cli()
+    BASE_DIR = args['dataset']
+    fvc_train = read_fvc_data(BASE_DIR + 'train.csv')
+
+    # get rid of bad images of this patient
+    fvc_train = fvc_train[fvc_train['Patient'] != 'ID00052637202186188008618']
+
+    TRAIN_IMG_DIR = BASE_DIR + 'train/'
+    train = iut.prepare_dataset(fvc_train, TRAIN_IMG_DIR, train=True)
+
+    model = utils.build_model(
+        (500, 500)
+    )
+
+    history = model.fit(train, epochs=2, steps_per_epoch=len(fvc_train))
