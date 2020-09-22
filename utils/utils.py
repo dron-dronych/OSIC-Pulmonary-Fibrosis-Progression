f8513f3e56ab5cb73c0c184c72a8194b535c8ae1
dron-dronych <dr_dronych@mail.ru>
2020-09-22 23:11:04 +0600

    conv net with vgg16 base

--- utils/utils.py
+++ utils/utils.py
@@ -54,9 +54,69 @@ def build_conv_net(input_shape):
     inputs = layers.Input(shape=input_shape)
     x = layers.Conv2D(32, (3, 3))(inputs)
     x = layers.Conv2D(3, (3, 3))(x)
-    # TODO check why it throws OOM on this layer
+    # TODO check https://github.com/dron-dronych/OSIC-Pulmonary-Fibrosis-Progression/issues/3
     x = layers.Dense(128, activation='relu')(x)
     x = layers.Dense(64, activation='relu')(x)
+    preds = layers.Dense(1, activation='linear')(x)
+
+    model = keras.Model(inputs=[inputs],
+                        outputs=[preds]
+                        )
+    model.compile(optimizer='adam',
+                  loss=keras.losses.MeanAbsoluteError(),
+                  metrics=laplace_log_likelihood
+                  )
+
+    return model
+
+
+def build_conv_net_vgg16base(input_shape):
+    inputs = layers.Input(shape=input_shape)
+
+    # Block 1
+    x = layers.Conv2D(
+        64, (3, 3), activation='relu', padding='same', name='block1_conv1')(
+        inputs)
+    x = layers.Conv2D(
+        64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+    # Block 2
+    x = layers.Conv2D(
+        128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = layers.Conv2D(
+        128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = layers.Conv2D(
+        256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = layers.Conv2D(
+        256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = layers.Conv2D(
+        256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = layers.Conv2D(
+        512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = layers.Conv2D(
+        512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = layers.Conv2D(
+        512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = layers.Conv2D(
+        512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = layers.Conv2D(
+        512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+    x = layers.Conv2D(
+        512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+    x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+    x = layers.Dense(32, activation='relu')(x)
+
     preds = layers.Dense(1, activation='linear')(x)
 
     model = keras.Model(inputs=[inputs],
